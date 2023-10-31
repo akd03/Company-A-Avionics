cb5649ac5f9715bb6d4fc3d6f73190dc052303a5
Matthew Barrat <149067797+gv22708@users.noreply.github.com>
2023-10-31 17:10:09 +0000

    Add files via upload
    Finalised text class and full futura font set

---
+++ text.py
@@ -0,0 +1,16 @@
+class Texts():
+    def __init__(self, contents, font, colour, x, y):
+        #creating the text object
+        self.text_object = font.render(contents, 1, colour)
+        #creating a rectangle from the object for pygame to place
+        self.rectangle = self.text_object.get_rect(center=(x,y))
+
+    def display(self, surface):
+        surface.blit(self.text_object, (self.rectangle.x, self.rectangle.y))
+
+# Example font
+#fontname = pygame.font.Font(None, fontsize) gives default font
+#fontname = pygame.font.Font(filename, fontsize)
+
+# Example object
+#text = Texts("Contents", font, (255, 255, 255), screen, x, y)
